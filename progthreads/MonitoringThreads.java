d0b902cf6c4535c00da4a9b53e94ae06af03178b
Igor N Faustino <igornfaustino@gmail.com>
2019-03-31 23:36:00 -0300

    add more 4 exercises about interruption

---
+++ progthreads/MonitoringThreads.java
@@ -0,0 +1,68 @@
+package progthreads;
+
+import java.util.ArrayList;
+
+public class MonitoringThreads extends Thread {
+    ArrayList<Thread> threads;
+
+    MonitoringThreads(ArrayList threads) {
+        this.threads = threads;
+    }
+
+    @Override
+    public void run() {
+        while (threads.size() > 0) {
+            for (int i = 0; i < threads.size(); i++) {
+                if (threads.get(i).isAlive()) {
+                    if (threads.get(i).isInterrupted()) {
+                        System.out.println(threads.get(i).getName() + " was interrupted");
+                    }
+                } else {
+                    // threads.remove(i);
+                }
+            }
+            if (Thread.currentThread().isInterrupted()) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Thread> threads = new ArrayList<Thread>();
+        threads.add(new Thread(new TimeRunnable()));
+        threads.add(new Thread(new TimeRunnable()));
+        threads.add(new Thread(new TimeRunnable()));
+
+        Thread monitor = new MonitoringThreads(threads);
+        monitor.start();
+
+        threads.get(0).start();
+        threads.get(1).start();
+        threads.get(2).start();
+
+        try {
+            threads.get(0).interrupt();
+            Thread.sleep(1000);
+            threads.get(1).interrupt();
+            Thread.sleep(1000);
+            threads.get(2).interrupt();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        monitor.interrupt();
+    }
+}
+
+class TimeRunnable implements Runnable {
+    @Override
+    public void run() {
+        try {
+            long time = (long) (10000);
+            Thread.sleep(time);
+            System.out.println(Thread.currentThread().getName() + " is dead after " + time + " ms");
+        } catch (InterruptedException e) {
+            // System.out.println("thread is interrupted");
+        }
+    }
+}
---
+++ progthreads/ReadFileThread.java
@@ -0,0 +1,48 @@
+package progthreads;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/**
+ * Faca uma thread Java que realize a leitura de um arquivotexto com frases e
+ * exiba as frases a cada 10 segundos.
+ *
+ * @author igornfaustino
+ */
+
+public class ReadFileThread {
+    public static void main(String[] args) {
+    
+        new Thread(new ReadFileRunnable("progthreads/file.txt")).start();
+    }
+}
+
+class ReadFileRunnable implements Runnable {
+    BufferedReader reader;
+
+    ReadFileRunnable(String filePath) {
+        try {            
+            this.reader = new BufferedReader(new FileReader(filePath));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        String strCurrentLine;
+        try {
+            while ((strCurrentLine = reader.readLine()) != null) {
+                try {
+                    System.out.println(strCurrentLine);
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    System.out.println("Thread interrupted");
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
